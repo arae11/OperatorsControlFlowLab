6b1a012801f150c016b8ac2a18aa27ace0653bd6
Alex Rae <arae@spartaglobal.com>
2021-07-30 09:18:28 +0100

    Finished Methods + Tests

---
+++ Op_CtrlFlow_Lib/Exercises.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+
+namespace Op_CtrlFlow
+{
+    public class Exercises
+    {
+        public static bool MyMethod(int num1, int num2)
+        {
+            return num1 == num2 ? false : (num1 % num2) == 0;
+        }
+
+        // returns the average of the array nums
+        public static double Average(List<int> nums)
+        {
+            double average = 0;
+            try
+            {
+                if (nums.Count > 0)
+                {
+                    average = nums.Average();
+                }
+                return average;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new ArgumentException("Index is out of range", nameof(nums), ex);
+            }
+        }
+
+        // returns the type of ticket a customer is eligible for based on their age
+        // "Standard" if they are between 18 and 59 inclusive
+        // "OAP" if they are 60 or over
+        // "Student" if they are 13-17 inclusive
+        // "Child" if they are 5-12
+        // "Free" if they are under 5
+        public static string TicketType(int age)
+        {
+            string ticketType = string.Empty;
+            if (age < 5)
+            {
+                ticketType = "Free";
+            }
+            else if (age < 13)
+            {
+                ticketType = "Child";
+            }
+            else if (age < 18)
+            {
+                ticketType = "Student";
+            }
+            else if (age >= 60)
+            {
+                ticketType = "OAP";
+            }
+            else
+            {
+                ticketType = "Standard";
+            }
+            return ticketType;
+        }
+
+        public static string Grade(int mark)
+        {
+            var grade = "";
+            if (mark >= 75 && mark <= 100)
+            {
+                grade = "Pass with Distinction";
+                throw new ArgumentOutOfRangeException("Mark: " + mark + ", Allowed range of 0-100");
+            }
+            else if (mark >= 60 && mark <= 74)
+            {
+                grade = "Pass with Merit";
+                throw new ArgumentOutOfRangeException("Mark: " + mark + ", Allowed range of 0-100");
+            }
+            else if (mark >= 40 && mark <= 59)
+            {
+                grade = "Pass";
+                throw new ArgumentOutOfRangeException("Mark: " + mark + ", Allowed range of 0-100");
+            }
+            else
+            {
+                grade = "Fail";
+            }
+            return grade;
+        }
+
+        public static int GetScottishMaxWeddingNumbers(int covidLevel)
+        {
+            switch (covidLevel)
+            {
+                case 4:
+                    covidLevel = 20;
+                    break;
+                case 3:
+                    covidLevel = 50;
+                    break;
+                case 2:
+                    covidLevel = 50;
+                    break;
+                case 1:
+                    covidLevel = 100;
+                    break;
+                case 0:
+                    covidLevel = 200;
+                    break;
+            }
+            return covidLevel;
+        }
+    }
+}
---
+++ Op_CtrlFlow_Tests/Exercises_Tests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using Op_CtrlFlow;
+using System.Collections.Generic;
+
+namespace Op_CtrlFlow_Tests
+{
+    public class Exercises_Tests
+    {
+        // write unit test(s) for MyMethod here
+        [TestCase(10, 10, false)]
+        [TestCase(10, 20, false)]
+        [TestCase(20, 10, true)]
+        public void MyMethodTest(int num1, int num2, bool expected)
+        {
+            Assert.AreEqual(expected, Exercises.MyMethod(num1, num2));
+        }
+
+        [Test]
+        public void Average_ReturnsCorrectAverage()
+        {
+            var myList = new List<int>() { 3, 8, 1, 7, 3 };
+            Assert.That(Exercises.Average(myList), Is.EqualTo(4.4));
+        }
+
+        [Test]
+        public void WhenListIsEmpty_Average_ReturnsZero()
+        {
+            var myList = new List<int>() {};
+            Assert.That(Exercises.Average(myList), Is.EqualTo(0));
+        }
+
+        [TestCase(100, "OAP")]
+        [TestCase(60, "OAP")]
+        [TestCase(59, "Standard")]
+        [TestCase(18, "Standard")]
+        [TestCase(17, "Student")]
+        [TestCase(13, "Student")]
+        [TestCase(12, "Child")]
+        [TestCase(5, "Child")]
+        [TestCase(4, "Free")]
+        [TestCase(0, "Free")]
+        public void TicketTypeTest(int age, string expected)
+        {
+            var result = Exercises.TicketType(age);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(100, "Pass with Distinction")]
+        [TestCase(75, "Pass with Distinction")]
+        [TestCase(74, "Pass with Merit")]
+        [TestCase(60, "Pass with Merit")]
+        [TestCase(59, "Pass")]
+        [TestCase(40, "Pass")]
+        [TestCase(39, "Fail")]
+        [TestCase(0, "Fail")]
+        public void GradeTypeTest(int mark, string expected)
+        {
+            var result = Exercises.Grade(mark);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase(4, 20)]
+        [TestCase(3, 50)]
+        [TestCase(2, 100)]
+        [TestCase(1, 200)]
+        public void CovidLevelTest(int covidlevel, int attendees)
+        {
+            var result = Exercises.GetScottishMaxWeddingNumbers(covidlevel);
+            Assert.That(result, Is.EqualTo(result));
+        }
+
+    }
+}
